a14b7318bcf9161780b45dbba678543483c96dbe
jefsantos <jsantos@apoioprodesp.sp.gov.br>
2019-11-07 16:16:18 -0300

    ajuste (id_passeio...nome_passeio)

--- src/main/java/com/omelhordochile/model/PasseioModel.java
+++ src/main/java/com/omelhordochile/model/PasseioModel.java
@@ -21,56 +21,56 @@ public class PasseioModel implements Serializable{
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	int id;
-	String nome;
-	String valor;
+	int id_passeio;
+	String nome_passeio;
+	String valor_passeio;
 	
 	public PasseioModel() {
 		
 	}
 	
 	
-	public PasseioModel(int id, String nome, String valor) {
+	public PasseioModel(int id_passeio, String nome_passeio, String valor_passeio) {
 		super();
-		this.id = id;
-		this.nome = nome;
-		this.valor = valor;
+		this.id_passeio = id_passeio;
+		this.nome_passeio = nome_passeio;
+		this.valor_passeio = valor_passeio;
 	}
 
 
-	public int getId() {
-		return id;
+	public int getId_passeio() {
+		return id_passeio;
 	}
 
 
-	public void setId(int id) {
-		this.id = id;
+	public void setId_passeio(int id_passeio) {
+		this.id_passeio = id_passeio;
 	}
 
 
-	public String getNome() {
-		return nome;
+	public String getNome_passeio() {
+		return nome_passeio;
 	}
 
 
-	public void setNome(String nome) {
-		this.nome = nome;
+	public void setNome_passeio(String nome_passeio) {
+		this.nome_passeio = nome_passeio;
 	}
 
 
-	public String getValor() {
-		return valor;
+	public String getValor_passeio() {
+		return valor_passeio;
 	}
 
 
-	public void setValor(String valor) {
-		this.valor = valor;
+	public void setValor_passeio(String valor_passeio) {
+		this.valor_passeio = valor_passeio;
 	}
 
 
 	@Override
 	public String toString() {
-		return "PasseioModel [id=" + id + ", nome=" + nome + ", valor=" + valor + "]";
+		return "PasseioModel [id_passeio=" + id_passeio + ", nome_passeio=" + nome_passeio + ", valor_passeio=" + valor_passeio + "]";
 	}
 	
 	
